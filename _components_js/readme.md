5fb707a46ed1fa4b03b50b5bb373067ba149dcd9
Eduardo Souza <eduardo.dasilva@canoastec.rs.gov.br>
2021-10-05 16:20:55 -0300

    4.2 - Parametrizando Projeto

--- _components_js/readme.md
+++
@@ -1,4 +0,0 @@
-Modulo 3 - Aula 2
-Modulo 3 - Aula 3
-Modulo 3 - Aula 4
-Modulo 3 - Aula 5
---
+++ _project_imob/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap Builder</title>
+
+    <link rel="stylesheet" href="_cdn/css/bootstrap_custom.css">
+    <link rel="stylesheet" href="_cdn/css/app.css">
+</head>
+<body>
+    <button class="btn btn-front icon-info">Teste</button>
+
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
