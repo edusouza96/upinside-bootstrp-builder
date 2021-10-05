4a2582ec92526fe5e95883b0893b96ee22f73e05
Eduardo Souza <eduardo.dasilva@canoastec.rs.gov.br>
2021-10-05 16:42:09 -0300

    4.3 - Desenvolvendo Topo

--- _project_imob/index.php
+++ _project_imob/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,10 +10,59 @@
     <link rel="stylesheet" href="_cdn/css/bootstrap_custom.css">
     <link rel="stylesheet" href="_cdn/css/app.css">
 </head>
+
 <body>
-    <button class="btn btn-front icon-info">Teste</button>
+    <header class="main_header">
+        <div class="header_bar bg-front">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4 d-flex justify-content-center align-items-center p-2 text-white">
+                        <i class="icon-location-arrow"></i>
+                        <p class="my-auto ml-3">Avenida Pequeno Principe, 0 Campeche <br>Florianópolis/SC</p>
+                    </div>
+
+                    <div class="col-4 d-flex justify-content-center align-items-center p-2 text-white">
+                        <i class="icon-clock-o"></i>
+                        <p class="my-auto ml-3">Seg/Sex: 09h00h - 18:00h <br>Sáb/Dom: Plantão</p>
+                    </div>
+
+                    <div class="col-4 d-flex justify-content-center align-items-center p-2 text-white">
+                        <i class="icon-envelope"></i>
+                        <p class="my-auto ml-3">contato@upinside.com.br<br>+55 (48) 3322-1234</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <nav class="navbar navbar-expand-md navbar-light my-3">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a href="index.php">
+                        <h1 class="text-hide">Imobiliária</h1>
+                        <img src="assets/images/logo.png" alt="" class="d-inline-block" width="280">
+                    </a>    
+                </div>
+                
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+    
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                        <li class="nav-item"><a class="nav-link text-front" href="#">Destaque</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Alugar</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Comprar</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Contato</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
 
     <script src="node_modules/jquery/dist/jquery.min.js"></script>
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
--- _project_imob/readme.md
+++ _project_imob/readme.md
@@ -1,1 +1,2 @@
 Modulo 4 - Aula 2
+Modulo 4 - Aula 3
