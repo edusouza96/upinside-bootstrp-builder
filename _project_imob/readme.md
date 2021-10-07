72e63c399663ceb24061104b0954cbd19cc47e23
Eduardo Souza <eduardo.dasilva@canoastec.rs.gov.br>
2021-10-07 17:48:58 -0300

    4.7 - [Home] Dobra Listagem

--- _project_imob/readme.md
+++ _project_imob/readme.md
@@ -3,3 +3,4 @@ Modulo 4 - Aula 3
 Modulo 4 - Aula 4
 Modulo 4 - Aula 5
 Modulo 4 - Aula 6
+Modulo 4 - Aula 7
--- _project_imob/widget/home.php
+++ _project_imob/widget/home.php
@@ -100,7 +100,7 @@
             <p class="text-center text-muted h4">Encontre um imóvel com a experiência que você quer viver</p>
         </div>
 
-        <div class="main_list_group_items mt-5 d-fles justify-content-around row">
+        <div class="main_list_group_items mt-5 d-flex justify-content-around row">
             <article class="main_list_group_items_item col-4 mb-4">
                 <a href="#">
                     <div class="d-flex align-items-center justify-content-center" style="background: url('assets/images/home/cobertura_oto_1.jpg') no-repeat; background-size: cover;">
@@ -147,6 +147,233 @@
                         <h2>Lojas e salas</h2>
                     </div>
                 </a>
+            </article>
+        </div>
+    </div>
+</section>
+
+<section class="main_properties py-5">
+    <div class="container">
+        <div class="border-bottom d-flex justify-content-between align-items border-front mb-5">
+            <h1 class="text-front">À Venda</h1>
+            <a href="#" class="text-front">Ver mais</a>
+        </div>
+
+        <div class="row">
+            <article class="col-4">
+                <div class="card main_properties_item">
+                    <div class="img-responsive-16by9">
+                        <a href="">
+                            <img src="properties/1/089dac35-8d59-4cb0-bf0a-7b9a7d8642f1.jpg" class="card-img-top" alt="">
+                        </a>
+                    </div>
+
+                    <div class="card-body">
+                        <h2><a href="" class="text-front">Linda casa no campeche com vista para o Mar</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                        <a href="" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+                    <div class="card-footer text-muted d-flex">
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/bed.png"  class="img-fluid" alt="">
+                            <p class="text-muted">1</p>
+                        </div>
+
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/garage.png"  class="img-fluid" alt="">
+                            <p class="text-muted">4</p>
+                        </div>
+
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/util-area.png"  class="img-fluid" alt="">
+                            <p class="text-muted">180m²</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+
+            <article class="col-4">
+                <div class="card main_properties_item">
+                    <div class="img-responsive-16by9">
+                        <a href="">
+                            <img src="properties/2/7949553b-6a91-4acf-a2a2-0b9560b2eff3.jpg" class="card-img-top" alt="">
+                        </a>
+                    </div>
+
+                    <div class="card-body">
+                        <h2><a href="" class="text-front">Linda casa no campeche com vista para o Mar</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                        <a href="" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+                    <div class="card-footer text-muted d-flex">
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/bed.png"  class="img-fluid" alt="">
+                            <p class="text-muted">1</p>
+                        </div>
+
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/garage.png"  class="img-fluid" alt="">
+                            <p class="text-muted">4</p>
+                        </div>
+
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/util-area.png"  class="img-fluid" alt="">
+                            <p class="text-muted">180m²</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+
+            <article class="col-4">
+                <div class="card main_properties_item">
+                    <div class="img-responsive-16by9">
+                        <a href="">
+                            <img src="properties/3/48af3f47-4fdc-461c-ba7b-39eb759aba71.jpg" class="card-img-top" alt="">
+                        </a>
+                    </div>
+
+                    <div class="card-body">
+                        <h2><a href="" class="text-front">Linda casa no campeche com vista para o Mar</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 400.000,00</p>
+                        <a href="" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+                    <div class="card-footer text-muted d-flex">
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/bed.png"  class="img-fluid" alt="">
+                            <p class="text-muted">1</p>
+                        </div>
+
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/garage.png"  class="img-fluid" alt="">
+                            <p class="text-muted">4</p>
+                        </div>
+
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/util-area.png"  class="img-fluid" alt="">
+                            <p class="text-muted">180m²</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>
+</section>
+
+
+<section class="main_properties py-5 bg-light">
+    <div class="container">
+        <div class="border-bottom d-flex justify-content-between align-items border-front mb-5">
+            <h1 class="text-front">Para alugar</h1>
+            <a href="#" class="text-front">Ver mais</a>
+        </div>
+
+        <div class="row">
+            <article class="col-4">
+                <div class="card main_properties_item">
+                    <div class="img-responsive-16by9">
+                        <a href="">
+                            <img src="properties/4/3b9d6713-3e86-4d41-a21b-6b77fc414bf3.jpg" class="card-img-top" alt="">
+                        </a>
+                    </div>
+
+                    <div class="card-body">
+                        <h2><a href="" class="text-front">Linda casa no campeche com vista para o Mar</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 4.000,00/mês</p>
+                        <a href="" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+                    <div class="card-footer text-muted d-flex">
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/bed.png"  class="img-fluid" alt="">
+                            <p class="text-muted">1</p>
+                        </div>
+
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/garage.png"  class="img-fluid" alt="">
+                            <p class="text-muted">4</p>
+                        </div>
+
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/util-area.png"  class="img-fluid" alt="">
+                            <p class="text-muted">180m²</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+
+            <article class="col-4">
+                <div class="card main_properties_item">
+                    <div class="img-responsive-16by9">
+                        <a href="">
+                            <img src="properties/5/0ed8b84d-67c1-4688-86de-433ec3a498d5.jpg" class="card-img-top" alt="">
+                        </a>
+                    </div>
+
+                    <div class="card-body">
+                        <h2><a href="" class="text-front">Linda casa no campeche com vista para o Mar</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 2.000,00/mês</p>
+                        <a href="" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+                    <div class="card-footer text-muted d-flex">
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/bed.png"  class="img-fluid" alt="">
+                            <p class="text-muted">1</p>
+                        </div>
+
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/garage.png"  class="img-fluid" alt="">
+                            <p class="text-muted">4</p>
+                        </div>
+
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/util-area.png"  class="img-fluid" alt="">
+                            <p class="text-muted">180m²</p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+
+            <article class="col-4">
+                <div class="card main_properties_item">
+                    <div class="img-responsive-16by9">
+                        <a href="">
+                            <img src="properties/2/e65a8436-fd1c-4db7-bf50-ece54a58b6a7.jpg" class="card-img-top" alt="">
+                        </a>
+                    </div>
+
+                    <div class="card-body">
+                        <h2><a href="" class="text-front">Linda casa no campeche com vista para o Mar</a></h2>
+                        <p class="main_properties_item_category">Imóvel Residencial</p>
+                        <p class="main_properties_item_type">Apartamento - Campeche <i class="icon-location-arrow"></i></p>
+                        <p class="main_properties_item_price text-front">R$ 1.500,00/mês</p>
+                        <a href="" class="btn btn-front btn-block">Ver Imóvel</a>
+                    </div>
+                    <div class="card-footer text-muted d-flex">
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/bed.png"  class="img-fluid" alt="">
+                            <p class="text-muted">1</p>
+                        </div>
+
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/garage.png"  class="img-fluid" alt="">
+                            <p class="text-muted">4</p>
+                        </div>
+
+                        <div class="col-4 main_properties_item_features text-center">
+                            <img src="assets/images/icons/util-area.png"  class="img-fluid" alt="">
+                            <p class="text-muted">180m²</p>
+                        </div>
+                    </div>
+                </div>
             </article>
         </div>
     </div>
